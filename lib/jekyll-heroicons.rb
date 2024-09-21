deabe94db9faca2e759f7489cbeb0b720925f370
Stanislav (Stas) Katkov <github@skatkov.com>
2024-09-21 03:12:06 +0200

    one more fix

--- lib/jekyll-heroicons.rb
+++ lib/jekyll-heroicons.rb
@@ -24,13 +24,6 @@ module Jekyll
       super
       @markup = markup
 
-      @symbol = symbol(markup)
-      @variant  = if (match = markup.split('/')).length > 1
-                    match.first
-                  else
-                    'solid'
-                  end
-
       # If there's interpolation going on, we need to do this in render
       prepare(markup) unless markup.match(Variable)
     end
