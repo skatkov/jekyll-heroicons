5fe41c6c4c9c9dd7204ae760428983551669f5ec
Stanislav (Stas) Katkov <github@skatkov.com>
2024-09-21 02:52:36 +0200

    better improvement

--- test/test_heroicons.rb
+++ test/test_heroicons.rb
@@ -17,4 +17,21 @@ class JekyllHeroiconsTest < Minitest::Test
 
     assert_equal expected_content, output
   end
+
+  def test_render_nothing
+    assert_equal '', render("{% heroicon %}")
+  end
+
+  def test_parses_tag_options
+    output = render("{% heroicon arrow-up height:32 class:\"left right\" aria-label:hi%}")
+
+    assert_match /height="32"/, output
+    assert_match /class="[^"]+ left right/, output
+    assert_match /aria-label="hi"/, output
+  end
+
+  def test_parses_interpolation_of_variables
+    template = render("{% assign symbol = \"solid/arrow-up\" %}{% heroicon {{ symbol }} %}")
+    assert_match /<svg.*.*/, template
+  end
 end
